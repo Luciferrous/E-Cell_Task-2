c857c10c34655bb272c03f5de8fcf4fb30df7558
Tushar00002 <102597162+Tushar00002@users.noreply.github.com>
2022-12-31 17:56:26 +0530

    Created basic App

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # E-Cell_Task-2
+
+put all required images in assets/images
+create your page in component section
+Layout : Put all the pages in decided oeder
---
+++ e_cell/src/Layout.js
@@ -0,0 +1,14 @@
+//Put all your pages here in decide order 
+
+import React from 'react'
+
+
+const Layout = () => {
+  return (
+    <div className='w-full h-screen'>
+     
+    </div>
+  )
+}
+
+export default Layout
