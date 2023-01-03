bbd25ffbd90c88d37c4d2d16075d0c9a786d4499
Chandni <chandnik96.40@gmail.com>
2023-01-03 16:19:02 +0530

    Changed the animation in Startups page

--- e_cell/src/components/Startup.js
+++ e_cell/src/components/Startup.js
@@ -8,7 +8,6 @@ const Startups=(props)=>{
   return(
     <>
     
-<<<<<<< HEAD
     <div className=" h-80 border flex  rounded-2xl group bg-white m-4 p-2 mb-10 pb-16">
       <div className=" hover:scale-110 flex flex-row lg:block">
     {/* <h1 className="text-white text-3xl font-bold">{props.title}</h1> */}
@@ -17,15 +16,7 @@ const Startups=(props)=>{
     </div>
     <div className="md:basis-1/4 h-full w-full bg-black -z-index-1 hover:z-index-3 flex items-center justify-center  opacity-0 group-hover:opacity-100 overflow-hidden transition-all border rounded-3xl duration-300 text-cyan-400 font-xl font-bold">
     <p className="relative p-2 m-2 -z-index-1 hover:z-index-3 flex  w-full h-full ">{props.about}</p>
-=======
-    <div className="w-140 h-80 border flex rounded-2xl group bg-white m-4 p-2">
-      <div className=" flex flex-row ">
-    {/* <h1 className="text-white text-3xl font-bold">{props.title}</h1> */}
-    <img className="w-60 h-full flex self-center" src={props.images} alt={props.title}/>
->>>>>>> 2d9def35de027a49e40d9ca764d4ddacd8a13c5f
     </div>
-    <div className=" h-full w-full bg-black flex items-center justify-center  opacity-0 group-hover:opacity-100 overflow-hidden transition-all duration-300 text-cyan-400 font-xl font-bold">
-    <p>{props.about}</p>
     </div>
     </div>
     </>
