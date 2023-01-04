f652fb6aadbb6d6a9829020b19c98a66e060133e
Tushar00002 <102597162+Tushar00002@users.noreply.github.com>
2023-01-04 12:29:32 +0530

    Edited startup

--- e_cell/src/components/Start.js
+++ e_cell/src/components/Start.js
@@ -25,7 +25,7 @@ const Start = () => {
                            <img className='w-[250px]'  src={data[0].images}></img>
                         </div>
                         
-                        <div className={` text-black ${!val1?'hidden':''} bg-sec h-[250px]`}>
+                        <div className={`${!val1?'hidden':''} bg-sec h-[250px] p-2 text-justify text-white `}>
                           <p >{data[0].about}</p>
                         </div>
                        
@@ -42,7 +42,7 @@ const Start = () => {
                            <img className='w-[250px]' src={data[1].images}></img>
                         </div>
                         
-                        <div className={` text-black ${!val2?'hidden':''} bg-sec h-[250px]`}>
+                        <div className={`${!val2?'hidden':''} bg-sec h-[250px] p-2 text-justify text-white `}>
                           <p >{data[1].about}</p>
                         </div>
                        
@@ -59,7 +59,7 @@ const Start = () => {
                            <img className='w-[250px]' src={data[2].images}></img>
                         </div>
                         
-                        <div className={` text-black ${!val3?'hidden':''} bg-sec h-[250px]`}>
+                        <div className={`${!val3?'hidden':''} bg-sec h-[250px] p-2 text-justify text-white `}>
                           <p >{data[2].about}</p>
                         </div>
                        
@@ -69,14 +69,14 @@ const Start = () => {
 
 
 
-                  <div className='Card bg-white w-[250px]'  onMouseEnter={()=>setval4(!val4)} onMouseLeave={()=>setval4(!val4)}>
+                  <div className='Card bg-white w-[250px] transform duration-300'  onMouseEnter={()=>setval4(!val4)} onMouseLeave={()=>setval4(!val4)}>
                      <div className='w-[250px] h-[250px]'>
                      <div className='info flex flex-col justify-around'>
-                        <div className={`w-[250px] hover:hidden ${val4?'hidden':''}` }>
+                        <div className={`w-[250px] ${val4?'hidden':''}` }>
                            <img className='w-[250px]' src={data[3].images}></img>
                         </div>
                         
-                        <div className={` text-black ${!val4?'hidden':''} bg-sec h-[250px]`}>
+                        <div className={`${!val4?'hidden':''} bg-sec h-[250px] p-2 text-justify text-white `}>
                           <p >{data[3].about}</p>
                         </div>
                        
@@ -90,11 +90,11 @@ const Start = () => {
                   <div className='Card bg-white w-[250px]' onMouseEnter={()=>setval5(!val5)} onMouseLeave={()=>setval5(!val5)}>
                      <div className='w-[250px] h-[250px]'>
                      <div className='info flex flex-col justify-around' >
-                        <div className={`w-[250px] hover:hidden ${val5?'hidden':''} flex ` }>
-                           <img className='w-[250px]' src={data[4].images}></img>
+                        <div className={`w-[250px] hover:hidden ${val5?'hidden':''} flex  items-center` }>
+                           <img className='w-[250px] ' src={data[4].images}></img>
                         </div>
                         
-                        <div className={` text-black ${!val5?'hidden':''} bg-sec h-[250px]`}>
+                        <div className={`${!val5?'hidden':''} bg-sec h-[250px]  p-2 text-justify text-white `}>
                           <p >{data[4].about}</p>
                         </div>
                        
